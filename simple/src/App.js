a07099aa1d92cae4df72dc84d35ec4e92816657f
mikiemapo <findmikemm@gmail.com>
2023-04-11 01:04:45 +0000

    save

---
+++ simple/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import SecondsCounter from './Components/ SecondsCounter';
+
+function App() {
+  return (
+    <>
+       
+       <SecondsCounter/>
+       
+    </>
+  );
+}
+
+export default App;
---
+++ simple/src/Components/ SecondsCounter.js
@@ -0,0 +1,7 @@
+const SecondsCounter = () => {
+    return(
+    <div className='header'>
+    <span className='title'>Counting seconds</span> </div>
+        )
+    }
+    export default SecondsCounter;
